20df9121bf15c89da583f20250625d84092adb4d
david-loe <56305409+david-loe@users.noreply.github.com>
2024-11-27 09:59:51 +0100

    Add dark mode

--- src/main.js
+++ src/main.js
@@ -6,4 +6,14 @@ import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+// Light/Dark Mode switch
+function updateBootstrapTheme() {
+    const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    document.documentElement.setAttribute('data-bs-theme', isDarkMode ? 'dark' : 'light');
+}
+// Initiales Setzen des Themes
+updateBootstrapTheme();
+// Event-Listener für Änderungen
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateBootstrapTheme);
+
 createApp(App).mount('#app')
