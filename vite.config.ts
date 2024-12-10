0dcdec7f1ac4b4b6a2776c5450da0bf1d6f184d7
david-loe <56305409+david-loe@users.noreply.github.com>
2024-12-10 13:55:51 +0100

    fix asset urls

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default {
-  plugins: [vue()],
-  server: { port: 8080, strictPort: true, host: '0.0.0.0' },
-  preview: { port: 8080, host: '0.0.0.0' },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    }
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    server: { port: 8080, strictPort: true, host: '0.0.0.0' },
+    preview: { port: 8080, host: '0.0.0.0' },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
+      }
+    },
+    base: mode === 'production' ? '/band-cues/' : '/'
   }
-}
+})
